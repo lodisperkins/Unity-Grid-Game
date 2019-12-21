9db937cf0ea3850abba78aa9dbb524a6578ecea8
lodisperkins44 <lodisperkins44@gmail.com>
2019-12-21 14:12:55 -0600

    Added energy bars to block menu

--- Assets/Scripts/Lodis/GamePlay/GridBehaviour.cs
+++ Assets/Scripts/Lodis/GamePlay/GridBehaviour.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 namespace Lodis.GamePlay
 {
---
+++ Assets/Scripts/Lodis/Movement/PlayerLookBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Lodis
+{
+	public class PlayerLookBehaviour : MonoBehaviour
+	{
+
+		[SerializeField] private Transform _enemyPosition;
+    	// Use this for initialization
+        private enum Facing
+        {
+	        Right = 0,
+	        Left = 180,
+	        Up = -90,
+	        Down = 90
+        }
+
+        private void RotatePlayerOnYAxis(float degrees)
+        {
+	        transform.rotation = Quaternion.Euler(transform.rotation.x,degrees,transform.rotation.z);
+        }
+        public void LookTowardsEnemyPosition()
+        {
+	        if ((int)_enemyPosition.position.z > (int)transform.position.z)
+	        {
+		       RotatePlayerOnYAxis((float)Facing.Right);
+	        }
+	        else if ((int) _enemyPosition.position.z < (int) transform.position.z)
+	        {
+		        RotatePlayerOnYAxis((float) Facing.Left);
+	        }
+        }
+    	// Update is called once per frame
+    	void Update () {
+    		LookTowardsEnemyPosition();
+    	}
+    }
+
+}
+
