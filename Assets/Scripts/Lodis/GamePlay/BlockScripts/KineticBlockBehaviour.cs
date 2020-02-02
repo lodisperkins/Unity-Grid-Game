3977c6fa44eea3efe04bf5e389421625a5931093
lodisperkins <42353525+lodisperkins@users.noreply.github.com>
2020-02-01 23:58:25 -0600

    Added kinetic block

---
+++ Assets/Scripts/Lodis/GamePlay/BlockScripts/KineticBlockBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Lodis
+{
+
+
+    public class KineticBlockBehaviour : MonoBehaviour {
+        List<Rigidbody> bullets;
+        List<Vector3> velocityVals;
+        [SerializeField]
+        HealthBehaviour block;
+	// Use this for initialization
+	void Start() {
+            bullets = new List<Rigidbody>();
+            velocityVals = new List<Vector3>();
+        }
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Projectile"))
+            {
+                Rigidbody temp = other.GetComponent<Rigidbody>();
+                velocityVals.Add(temp.velocity);
+                bullets.Add(temp);
+                temp.velocity = Vector3.zero;
+            }
+        }
+        private void OnDestroy()
+        {
+            for (int i = 0; i < bullets.Count; i++)
+            {
+                bullets[i].AddForce(-(velocityVals[i]), ForceMode.Impulse);
+            }
+        }
+        // Update is called once per frame
+        void Update() {
+            if (bullets.Count > 10)
+            {
+                block.DestroyBlock(1);
+            }
+        }
+    }
+}
